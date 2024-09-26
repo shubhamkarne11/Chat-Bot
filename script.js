8b288ff502a7fbe9ce3302e39c1b3218c951e778
shubham karne <120401166+shubhamkarne11@users.noreply.github.com>
2024-09-26 22:10:30 +0530

    changes 2

--- script.js
+++ script.js
@@ -56,6 +56,31 @@ btn.addEventListener("click", () => {
     setTimeout(showLoading, 100);
 });
 
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function (e) {
+// disable F12 key
+
+if(e.keyCode ==123) {
+alert("F12 disable");
+  return false;
+}
+// disable c key
+if(e.ctrlKey && e.keyCode == 67) { 
+    alert("ctrl + c disable");
+return false;
+
+}
+
+//disable U key
+if(e.ctrlKey && e.keyCode == 85) { 
+    alert("ctrl + u disable");
+    return false;
+
+}
+
+
+}
+
 
 
 
